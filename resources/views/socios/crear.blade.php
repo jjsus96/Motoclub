970b3bb28ac407fd5772e07347bdc4383797c113
jjsus96 <jesus.sayago.mey.1996@gmail.com>
2022-05-02 21:56:29 +0200

    restablecimiento de contraseña funcionando

---
+++ resources/views/socios/crear.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <tittle>Laravel<tittle/>
+        <br>
+        <style>
+            body {
+                margin: auto;
+                padding: 50px;
+            }
+            input[type=text], select {
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+            }
+            input[type=submit] {
+                width: 100%;
+                background-color: #4CAF50;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+            }
+            input[type=submit]:hover {
+                background-color: #45a049;
+            }
+            div {
+                border-radius: 5px;
+                background-color: #f2f2f2;
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <a href="/users">Ver listado de usuarios</a>
+        <br>
+        <h2>Nuevo usuario</h2>
+        <div>
+            <form action="/users/crear" method ="POST">
+                @csrf
+                <label>Nombre:</label>
+                <input type="text" name="name" placeholder="Nombre">
+                <label>Email:</label>
+                <input type="text" name="email" placeholder="Email">
+                <label>Contraseña:</label>
+                <input type="text" name="password" placeholder="Contraseña">
+                <input type="submit" value="Guardar">
+            </form>
+        </div>       
+    </body>
+<html>
---
+++ resources/views/socios/lista.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <tittle>Laravel<tittle/>
+        <style>
+            body {
+                margin: auto;
+                padding: 50px;
+            }
+
+            table, td, th {
+                border: 1px solid black;
+            }
+
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            th {
+                height: 70px;
+            }
+
+            td {
+                height: 30px;
+            }
+
+            .button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Listado usuarios</h2>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Contraseña</th>
+                <th>Acciones</th>
+            </tr>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->password }}</td>
+                    <td>
+                        <a href="/users/ver/{{$user->id}}">Ver</a>
+                        <a href="/users/editar/{{$user->id}}">Editar</a>
+                        <a href="/users/eliminar/{{$user->id}}" onclick="return eliminarUser('Eliminar Usuario')"> Eliminar</a>
+                    </td>
+                </tr>
+            @endforeach  
+        </table>  
+        <br>
+        <a href="/users/crear">Nuevo usuario</a>          
+    </body>
+
+    <script>
+        function eliminarUser(value) {
+            action = confirm(value) ? true : event.preventDefault()
+        }
+    </script>
+
+<html>
